8f8b9750c0273600f526b7cfe2d75665d344668a
amspring <am15501@163.com>
2017-11-15 20:04:28 +0800

    upgrade pip

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
-from flask.ext.script import Manager, Shell
-from flask.ext.migrate import Migrate, MigrateCommand
+from flask_script import Manager, Shell, Server
+from flask_migrate import Migrate, MigrateCommand
 from app import create_app, db
 from app.models import ArticleType, article_types, Source, \
     Comment, Article, User, Menu, ArticleTypeSetting, BlogInfo, \
@@ -35,7 +35,7 @@ manager.add_command("shell", Shell(make_context=make_shell_context))
 
 @manager.command
 def deploy(deploy_type):
-    from flask.ext.migrate import upgrade
+    from flask_migrate import upgrade
     from app.models import BlogInfo, User, ArticleTypeSetting, Source, \
         ArticleType, Plugin, BlogView, Comment
 
@@ -75,4 +75,5 @@ def deploy(deploy_type):
 
 
 if __name__ == '__main__':
+    manager.add_command("runserver", Server(host="0.0.0.0", port=9000, use_debugger=True, use_reloader=True))
     manager.run()
